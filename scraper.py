6950fecbeabe6ab6183f4d8ec0f0d549c5b08e45
Karishad <44437832+Karishad@users.noreply.github.com>
2022-03-12 11:03:51 -0600

    Added scraper.py file

---
+++ scraper.py
@@ -0,0 +1,15 @@
+# scraper.py
+# Ensure beautifulsoup is installed prior to running
+
+# Beautiful Soup is going to be our web scrapper
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+# sudo apt update
+# pip install beautifulsoup4
+
+# Requests is going to allow use to send HTTP requests to the webpages that we scrape
+# https://docs.python-requests.org/en/latest/
+# sudo apt update
+# pip install requests
+
+from bs4 import BeautifulSoup
+import requests
