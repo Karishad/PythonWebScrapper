10eb86a68dce21574f77f12ab82d6151c65fb000
Karishad <44437832+Karishad@users.noreply.github.com>
2022-03-17 19:13:39 -0500

    Added tkinter for GUI for webscrapper

--- scraper.py
+++ scraper.py
@@ -1,21 +1,27 @@
 # scraper.py
-# Ensure beautifulsoup is installed prior to running
 
 # Beautiful Soup is going to be our web scrapper
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-# sudo apt update
-# pip install beautifulsoup4
 
 # Requests is going to allow use to send HTTP requests to the webpages that we scrape
 # https://docs.python-requests.org/en/latest/
-# sudo apt update
-# pip install requests
+
+# tkinter is going to allow the creation of the GUI for the webscrapper
+# https://docs.python.org/3/library/tkinter.html
 
 from bs4 import BeautifulSoup
 import requests
+import tkinter as tk
+
+# setting up the GUI
+root = tk.Tk()
+message = tk.Label(root, text="Hello, World!")
+message.pack()
+root.mainloop()
 
 # url to scrape
 url = "https://en.wikipedia.org/wiki/Main_Page"
 request = requests.get(url)
 soup = BeautifulSoup(request.text, "html.parser")
-print(soup.title)
+print(soup.title.text)
+
